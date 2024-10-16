d2d2877ed9d8f747afdb17932874dc6aa84190f9
Md Reefat Jalil <142273925+Reefat004@users.noreply.github.com>
2024-10-16 13:50:16 -0400

    Update settings.py

--- realtime_chat/settings.py
+++ realtime_chat/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)pxjjo$*!%i^m2cmk1yke$o)^lv%yr@^txh7p1mn4n+11j2ty5'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
